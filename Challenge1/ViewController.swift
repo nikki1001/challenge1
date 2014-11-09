72fcd8b52aabea608c589dcf7cfe125c9addd910
Revanth Samavedam <revanth@Revanths-MacBook-Pro-2.local>
2014-11-09 00:02:10 -0500

    Submit challenge

---
+++ Challenge1/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Challenge1
+//
+//  Created by Revanth Samavedam on 11/8/14.
+//  Copyright (c) 2014 cornFalkes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBOutlet weak var buttonTextField: UITextField!
+    
+    @IBOutlet weak var sendMailButton: UIButton!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+   
+    
+    func resignfirstresponder () {
+        
+        messageTextField.resignFirstResponder()
+        nameTextField.resignFirstResponder()
+    }
+    
+    @IBAction func sendMailButton(sender: UIButton) {
+        
+        if nameTextField.text == "" || messageTextField.text == "" {
+            
+            messageLabel.hidden = false
+            messageLabel.textColor = UIColor.redColor()
+            messageLabel.text = " Enter both Name and Message"
+            resignFirstResponder()
+            
+            
+        } else {
+            
+            messageLabel.hidden = false
+            messageLabel.textColor = UIColor.greenColor()
+            messageLabel.text = "Message sent to " + nameTextField.text
+            nameTextField.resignFirstResponder()
+            sendMailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+            nameTextField.text = ""
+            messageTextField.text = ""
+            resignFirstResponder()
+            
+            
+        }
+        
+    }
+    
+    @IBAction func resetButton(sender: UIButton) {
+        
+        nameTextField.text = ""
+        messageTextField.text = ""
+        sendMailButton.setTitle("Send Mail", forState: UIControlState.Normal)
+        messageLabel.hidden = true
+        resignFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
